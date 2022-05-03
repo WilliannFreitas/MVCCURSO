da89b52dc81cd222ffbc0950b9e371a7f4c81368
Williann Carlos de Freitas <williann.de.freitas@gmail.com>
2022-05-03 16:04:14 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using MVCCURSO.Models;
+using System.Web.Mvc;
+
+namespace MVCCURSO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Willy",
+                Tipo = "Power"
+            };
+
+            //ex: 01
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewData["Nome"] = pessoa.Nome;
+            //ViewData["Tipo"] = pessoa.Tipo;
+
+            //ex: 02
+            //ViewBag.id = pessoa.PessoaId;
+            //ViewBag.nome = pessoa.Nome;
+            //ViewBag.tipo = pessoa.Tipo;
+
+            //ex: 03
+            return View(pessoa);
+        }
+
+        public ActionResult Contatos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lista(int PessoaId, string Nome, string Tipo)
+        {
+            ViewData["PessoaId"] = PessoaId;
+            ViewData["Nome"] = Nome;
+            ViewData["Tipo"] = Tipo;
+
+            return View();
+        }
+
+    }
+
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCURSO.Startup))]
+namespace MVCCURSO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
