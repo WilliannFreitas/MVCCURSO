a5399cf2ee6aea5d8ce9058c2ec26d7f27e0cc64
Williann Carlos de Freitas <williann.de.freitas@gmail.com>
2022-05-17 16:44:09 -0300

    commit 17/05

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,53 @@
+﻿using MVCCURSO.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCCURSO.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Usuario()
+        {
+            var usuario = new Usuario();
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Usuario(Usuario usuario)
+        {
+            //if (string.IsNullOrEmpty(usuario.Nome))
+            //{
+            //    ModelState.AddModelError("Nome", "Preencha o campo nome");
+            //}
+
+            //if (usuario.Senha != usuario.ConfirmarSenha)
+            //{
+            //    ModelState.AddModelError("", "As senhas são diferentes");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado",usuario);
+            }
+            return View(usuario);
+        }
+
+        public ActionResult Resultado(Usuario usuario)
+        {
+            return View(usuario);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var dbex = new Collection<string>
+            {
+                "Ana Luiza",
+                "Paula",
+                "Joelvis"
+            };
+
+            return Json(dbex.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MVCCURSO.Models
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "É necessario digitar no mínimo 5, e no máximo 10 caracteres")]
+        public string Nome { get; set; }
+        
+        public string Observacao { get; set; }
+
+        [Range(18, 70, ErrorMessage = "O limite de idade e de 18 a 70 anos")]
+        public int Idade { get; set; }
+
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Digite um email válido")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"[a-zA-Z]{5,15}", ErrorMessage = "É necessario digitar no mínimo 5, e no máximo 15 caracteres")]
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        [Remote("LoginUnico", "Usuario", ErrorMessage = "Este login já existe")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatório")]
+        public int Senha { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Senha", ErrorMessage = "As senhas são diferentes")]
+        public int ConfirmarSenha { get; set; }
+
+    }   
+}
